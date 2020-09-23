63200131c911f849ea501d9bf53910e528c10848
Marcelo Agoston <marceloagoston91@gmail.com>
2020-09-23 12:37:08 -0300

    Agregar Joins a todas las tablas implicadas; formatear resultado de la consulta

--- serves.js
+++ serves.js
@@ -13,23 +13,22 @@ const { Sequelize, QueryTypes } = require('sequelize');
 const moment = require('moment')
 const app = express()
 
-  /* conectarse a la BD */
-  const sequelize = new Sequelize('NBCH.Tracking', 'Usuario.NBCH.Tracking', 'Usuario.NBCH.Tracking', {
-    host: '10.250.0.75',
-    dialect: 'mssql',
-  });
+const sequelize = new Sequelize('NBCH.Tracking', 'Usuario.NBCH.Tracking', 'Usuario.NBCH.Tracking', {
+  host: '10.250.0.75',
+  dialect: 'mssql',
+});
 
 async function probarConsulta(){
     let fechaHoy = moment().format("YYYY-MM-DD");
-    const solicitudes = await sequelize.query("SELECT Sol.email, Sol.nombre_apellido, SE.id_estado FROM (solicitud_estados SE join solicitudes S on SE.id_solicitud = S.id) join solicitantes Sol on Sol.id = S.id_solicitante where SE.id_estado <> 3 AND DATEDIFF(day, SE.fecha,getdate()) > 4 ", { type: QueryTypes.SELECT });
-    /* const solicitudes = await sequelize.query("SELECT * from estados", { type: QueryTypes.SELECT }); */
+    const solicitudes = await sequelize.query(`SELECT Sol.email, Sol.nombre_apellido, SE.fecha, S.numero_solicitud, S.numero_tramite, E.descripcion AS Estado_Solicitud 
+                    FROM solicitud_estados SE 
+                    JOIN solicitudes S on SE.id_solicitud = S.id 
+                    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+                    JOIN estados E on E.id = SE.id_estado 
+                    WHERE SE.id_estado <> 3 
+                    AND DATEDIFF(day, SE.fecha,getdate()) > 4`
+    , { type: QueryTypes.SELECT });
+    /* const solicitudes = await sequelize.query("SELECT * from solicitud_estados"); */
     console.log(solicitudes);
 }
 probarConsulta();
-
-// Ruta en node
-app.get('/', function (req, res) {
-    res.send('Este es el <strong>home1221</strong>')
-    console.log('Pagina de inicio...')
-})
-app.listen(3030)
