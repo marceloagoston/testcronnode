976a0daf95810a3c1e9682fe4806449252e59b4c
Marcelo Agoston <marceloagoston91@gmail.com>
2020-09-24 13:45:09 -0300

    Agregar queries para obtener Total de pendientes y Total de atrasados

--- serves.js
+++ serves.js
@@ -20,15 +20,59 @@ const sequelize = new Sequelize('NBCH.Tracking', 'Usuario.NBCH.Tracking', 'Usuar
 
 async function probarConsulta(){
     let fechaHoy = moment().format("YYYY-MM-DD");
-    const solicitudes = await sequelize.query(`SELECT Sol.email, Sol.nombre_apellido, SE.fecha, S.numero_solicitud, S.numero_tramite, E.descripcion AS Estado_Solicitud 
+
+    const solicitudesPendientesTotales = await sequelize.query(`
+                    SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización 
                     FROM solicitud_estados SE 
                     JOIN solicitudes S on SE.id_solicitud = S.id 
                     JOIN solicitantes Sol on Sol.id = S.id_solicitante 
                     JOIN estados E on E.id = SE.id_estado 
                     WHERE SE.id_estado <> 3 
-                    AND DATEDIFF(day, SE.fecha,getdate()) > 4`
+                    GROUP BY S.sucursal_cercana`
     , { type: QueryTypes.SELECT });
-    /* const solicitudes = await sequelize.query("SELECT * from solicitud_estados"); */
-    console.log(solicitudes);
+
+    const solicitudesPendientesAtrasadas = await sequelize.query(`
+                    SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización
+                    FROM solicitud_estados SE 
+                    JOIN solicitudes S on SE.id_solicitud = S.id 
+                    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+                    JOIN estados E on E.id = SE.id_estado 
+                    WHERE SE.id_estado <> 3  AND DATEDIFF(day, SE.fecha,getdate()) > 4
+                    GROUP BY S.sucursal_cercana`
+    , { type: QueryTypes.SELECT });
+
+    /* const solicitudesJoin = await sequelize.query(`
+      
+      SELECT *
+      FROM solicitudes S1 LEFT OUTER JOIN (
+        SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización 
+        FROM solicitud_estados SE 
+        JOIN solicitudes S on SE.id_solicitud = S.id 
+        JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+        JOIN estados E on E.id = SE.id_estado 
+        WHERE SE.id_estado <> 3 
+        GROUP BY S.sucursal_cercana) 
+      ON S1.sucursal_cercana = S.sucursal_cercana
+      AS Pendientes LEFT OUTER JOIN
+
+      (SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización
+      FROM solicitud_estados SE 
+      JOIN solicitudes S on SE.id_solicitud = S.id 
+      JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+      JOIN estados E on E.id = SE.id_estado 
+      WHERE SE.id_estado <> 3  AND DATEDIFF(day, SE.fecha,getdate()) > 4
+      GROUP BY S.sucursal_cercana) AS Atrasadas
+      ON Pendientes.sucursal_cercana = Atrasadas.sucursal_cercana
+      `
+  , { type: QueryTypes.SELECT }); */
+
+    console.log('Solicitudes Totales:')
+    console.log(solicitudesPendientesTotales);
+    console.log('Solicitudes Atrasadas:')
+    console.log(solicitudesPendientesAtrasadas);
+   /*  console.log('Join')
+    console.log(solicitudesJoin); */
 }
+
+/* Pendientes.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización */
 probarConsulta();
