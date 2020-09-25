33538396826a9fa0bf04d2ec42d589926a436570
Marcelo Agoston <marceloagoston91@gmail.com>
2020-09-25 10:42:03 -0300

    Generar reporte final para enviar por mail

--- serves.js
+++ serves.js
@@ -18,61 +18,80 @@ const sequelize = new Sequelize('NBCH.Tracking', 'Usuario.NBCH.Tracking', 'Usuar
   dialect: 'mssql',
 });
 
-async function probarConsulta(){
-    let fechaHoy = moment().format("YYYY-MM-DD");
+async function generarReporte(){
+  let fechaHoy = moment().format("YYYY-MM-DD");
 
-    const solicitudesPendientesTotales = await sequelize.query(`
-                    SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización 
-                    FROM solicitud_estados SE 
-                    JOIN solicitudes S on SE.id_solicitud = S.id 
-                    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
-                    JOIN estados E on E.id = SE.id_estado 
-                    WHERE SE.id_estado <> 3 
-                    GROUP BY S.sucursal_cercana`
-    , { type: QueryTypes.SELECT });
+  const solicitudesPendientesTotales = await sequelize.query(`
+                  SELECT S.sucursal_cercana, count(*) AS cantidad_total_de_pendientes, max(S.updatedAt) AS ultima_actualización 
+                  FROM solicitud_estados SE 
+                  JOIN solicitudes S on SE.id_solicitud = S.id 
+                  JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+                  JOIN estados E on E.id = SE.id_estado 
+                  WHERE SE.id_estado <> 3 
+                  GROUP BY S.sucursal_cercana`
+  , { type: QueryTypes.SELECT });
 
-    const solicitudesPendientesAtrasadas = await sequelize.query(`
-                    SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización
-                    FROM solicitud_estados SE 
-                    JOIN solicitudes S on SE.id_solicitud = S.id 
-                    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
-                    JOIN estados E on E.id = SE.id_estado 
-                    WHERE SE.id_estado <> 3  AND DATEDIFF(day, SE.fecha,getdate()) > 4
-                    GROUP BY S.sucursal_cercana`
-    , { type: QueryTypes.SELECT });
-
-    /* const solicitudesJoin = await sequelize.query(`
-      
-      SELECT *
-      FROM solicitudes S1 LEFT OUTER JOIN (
-        SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización 
-        FROM solicitud_estados SE 
-        JOIN solicitudes S on SE.id_solicitud = S.id 
-        JOIN solicitantes Sol on Sol.id = S.id_solicitante 
-        JOIN estados E on E.id = SE.id_estado 
-        WHERE SE.id_estado <> 3 
-        GROUP BY S.sucursal_cercana) 
-      ON S1.sucursal_cercana = S.sucursal_cercana
-      AS Pendientes LEFT OUTER JOIN
-
-      (SELECT S.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización
+  const solicitudesPendientesAtrasadas = await sequelize.query(`
+      SELECT S.sucursal_cercana, count(*) AS pendientes_atrasadas , max(S.updatedAt) AS ultima_actualización
       FROM solicitud_estados SE 
       JOIN solicitudes S on SE.id_solicitud = S.id 
       JOIN solicitantes Sol on Sol.id = S.id_solicitante 
       JOIN estados E on E.id = SE.id_estado 
       WHERE SE.id_estado <> 3  AND DATEDIFF(day, SE.fecha,getdate()) > 4
-      GROUP BY S.sucursal_cercana) AS Atrasadas
-      ON Pendientes.sucursal_cercana = Atrasadas.sucursal_cercana
-      `
-  , { type: QueryTypes.SELECT }); */
+      GROUP BY S.sucursal_cercana `
+  , { type: QueryTypes.SELECT }); 
 
-    console.log('Solicitudes Totales:')
-    console.log(solicitudesPendientesTotales);
-    console.log('Solicitudes Atrasadas:')
-    console.log(solicitudesPendientesAtrasadas);
-   /*  console.log('Join')
-    console.log(solicitudesJoin); */
+  async function obtenerReporte(){
+      
+    let pendientes = await solicitudesPendientesTotales;
+    let atrasadas = await solicitudesPendientesAtrasadas;
+    let salida = [];
+    pendientes.forEach(loopTotales => {
+      atrasadas.forEach(loopAtrasadas => {
+        if(loopAtrasadas.sucursal_cercana == loopTotales.sucursal_cercana){
+          /* Si hay tareas atrasadas */
+          loopAtrasadas['cantidad_total_de_pendientes']=loopTotales.cantidad_total_de_pendientes
+          salida.push(loopAtrasadas);
+        } else {
+          /* Si no hay atrasadas */
+          loopTotales['pendientes_atrasadas']=0
+          salida.push(loopTotales);
+        }
+      });
+      
+    });
+    console.log(salida)    
+
+  }
+
+  console.log(obtenerReporte());
 }
+generarReporte();
 
-/* Pendientes.sucursal_cercana, count(*), max(S.updatedAt) AS Ultima_Actualización */
-probarConsulta();
+
+
+
+/* const solicitudesJoin = await sequelize.query(`
+  SELECT Pendientes.sucursal_cercana, Pendientes.Pendientes_Totales ,Atrasadas.Pendientes_Atrasadas
+  FROM(
+    SELECT S.sucursal_cercana, S.count(*) AS Pendientes_Totales, max(S.updatedAt) AS Ultima_Actualización 
+    FROM solicitud_estados SE 
+    JOIN solicitudes S on SE.id_solicitud = S.id 
+    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+    JOIN estados E on E.id = SE.id_estado 
+    WHERE SE.id_estado <> 3 
+    GROUP BY S.sucursal_cercana) Pendientes
+    
+    LEFT OUTER JOIN
+   
+    (
+    SELECT S.sucursal_cercana, count(*) AS Pendientes_Atrasadas , max(S.updatedAt) AS Ultima_Actualización
+    FROM solicitud_estados SE 
+    JOIN solicitudes S on SE.id_solicitud = S.id 
+    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
+    JOIN estados E on E.id = SE.id_estado 
+    WHERE SE.id_estado <> 3  AND DATEDIFF(day, SE.fecha,getdate()) > 4
+    GROUP BY S.sucursal_cercana) Atrasadas 
+    ON Pendientes.sucursal_cercana = Atrasadas.sucursal_cercana
+    `
+, { type: QueryTypes.SELECT }); */
