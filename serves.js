21b07202c59ed11537f6b12a07fdc016445c86e0
Marcelo Agoston <marceloagoston91@gmail.com>
2020-09-25 10:47:40 -0300

    Agregar cabecera actualizada

--- serves.js
+++ serves.js
@@ -4,13 +4,14 @@
 //Autor:  Sebastian Rodriguez
 //  linea  10 a 13  definicion de constantes de librerías
 //  linea  15 a 19  configuracion de sequalize (conexión con la db)
-//  linea  21 a 26  definición de la consulta para obtener mail de solicitantes que no hicieron actualizaciones en 4 dias o mas y cuyo estado no sea finalizado
-//  linea  27 ejecutar consulta descripta en la linea anterior
+//  linea  22 a 31 Consulta de Solicitudes pendientes para cada sucursal
+//  linea  33 a 41 Consultas de solicitudes con edad mayor a 4 dias
+//  linea  44 a 61 Funcion obtenerReporte() que genera el reporte a enviar por mail basandose en ambas consultas anteriores
+//  linea  68 Se invoca la funcion generarReporte() que ejecuta todo el codigo anterior
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 const express = require('express')
 const { Sequelize, QueryTypes } = require('sequelize');
-const moment = require('moment')
 const app = express()
 
 const sequelize = new Sequelize('NBCH.Tracking', 'Usuario.NBCH.Tracking', 'Usuario.NBCH.Tracking', {
@@ -19,7 +20,6 @@ const sequelize = new Sequelize('NBCH.Tracking', 'Usuario.NBCH.Tracking', 'Usuar
 });
 
 async function generarReporte(){
-  let fechaHoy = moment().format("YYYY-MM-DD");
 
   const solicitudesPendientesTotales = await sequelize.query(`
                   SELECT S.sucursal_cercana, count(*) AS cantidad_total_de_pendientes, max(S.updatedAt) AS ultima_actualización 
@@ -61,37 +61,8 @@ async function generarReporte(){
       
     });
     console.log(salida)    
-
   }
 
   console.log(obtenerReporte());
 }
 generarReporte();
-
-
-
-
-/* const solicitudesJoin = await sequelize.query(`
-  SELECT Pendientes.sucursal_cercana, Pendientes.Pendientes_Totales ,Atrasadas.Pendientes_Atrasadas
-  FROM(
-    SELECT S.sucursal_cercana, S.count(*) AS Pendientes_Totales, max(S.updatedAt) AS Ultima_Actualización 
-    FROM solicitud_estados SE 
-    JOIN solicitudes S on SE.id_solicitud = S.id 
-    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
-    JOIN estados E on E.id = SE.id_estado 
-    WHERE SE.id_estado <> 3 
-    GROUP BY S.sucursal_cercana) Pendientes
-    
-    LEFT OUTER JOIN
-   
-    (
-    SELECT S.sucursal_cercana, count(*) AS Pendientes_Atrasadas , max(S.updatedAt) AS Ultima_Actualización
-    FROM solicitud_estados SE 
-    JOIN solicitudes S on SE.id_solicitud = S.id 
-    JOIN solicitantes Sol on Sol.id = S.id_solicitante 
-    JOIN estados E on E.id = SE.id_estado 
-    WHERE SE.id_estado <> 3  AND DATEDIFF(day, SE.fecha,getdate()) > 4
-    GROUP BY S.sucursal_cercana) Atrasadas 
-    ON Pendientes.sucursal_cercana = Atrasadas.sucursal_cercana
-    `
-, { type: QueryTypes.SELECT }); */
